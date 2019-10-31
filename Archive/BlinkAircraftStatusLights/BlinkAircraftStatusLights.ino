c48f8692f234c725a03485f28b46011f6762116a
Nikhil Mishra <AvatarNikhil@users.noreply.github.com>
2019-10-31 23:53:51 +0530

    Kragon Program Structure Change

---
+++ Archive/BlinkAircraftStatusLights/BlinkAircraftStatusLights.ino
@@ -0,0 +1,54 @@
+// https://www.baldengineer.com/arduino-example-police-lights-with-millis.html
+
+// English for which LED to Strobe
+#define RED 0x0
+#define BLUE 0x1
+ 
+// Variable to track which LED is on
+byte whichLED = RED;
+ 
+// Where are the LEDs connected?
+const int LED_Red = 11;
+const int LED_Blue = 12;
+ 
+// State variables for the LEDs
+byte Red_State = LOW;
+byte Blue_State = LOW;
+ 
+// Some delay values to change flashing behavior
+unsigned long switchDelay = 250;
+unsigned long strobeDelay = 50;
+ 
+// Seed the initial wait for the strobe effect
+unsigned long strobeWait = strobeDelay;
+ 
+// Variable to see when we should swtich LEDs
+unsigned long waitUntilSwitch = switchDelay;  // seed initial wait
+ 
+void setup() {
+   pinMode(LED_Red, OUTPUT);
+   pinMode(LED_Blue, OUTPUT);
+}
+ 
+void loop() {
+    digitalWrite(LED_Red, Red_State);     // each iteration of loop() will set the IO pins,
+    digitalWrite(LED_Blue, Blue_State);    // even if they don't change, that's okay
+ 
+    // Toggle back and forth between the two LEDs
+    if ((long)(millis() - waitUntilSwitch)>=0) {
+        // time is up!
+        Red_State = LOW;
+        Blue_State = LOW;
+        whichLED = !whichLED;  // toggle LED to strobe
+        waitUntilSwitch += switchDelay;
+    }
+ 
+    // Create the stobing effect
+    if ((long)(millis() - strobeWait)>=0) {
+        if (whichLED == RED)
+            Red_State = !Red_State;
+        if (whichLED == BLUE)
+            Blue_State = !Blue_State;
+        strobeWait += strobeDelay;
+    }
+}
--- KRAGON-Computer/KRAGON-Computer.ino
+++ KRAGON-Computer/KRAGON-Computer.ino
@@ -6,202 +6,229 @@
  ****************/
 
 //Header Files
-#include "SimpleKalmanFilter.h"
-#include "Adafruit_BMP280.h"
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+//Radio
+#include <RF24Network.h>
+#include <RF24.h>
+#include <SPI.h>
+//Sonar
 #include <NewPing.h>
-#include <Wire.h>
 
-#define RLED 6// Green LED
-#define GLED 7// Green LED
-#define buzzer 8 
-
-// =============================================
-// ===          MISC Global Vars             ===
-// =============================================
-
-//Timers Vars
-unsigned long previousMillis = 0;
-unsigned long currentMillis;
-
-
-//bools
-bool release = false;
-bool landed = false;
-bool pyro = false;
-
-// =============================================
-// ===                SONAR                  ===
-// =============================================
-#define TRIGGER_PIN  12
-#define ECHO_PIN     9
-#define MAX_DISTANCE 400
+// ===================================================
+// ===                 MISC Vars                   ===
+// ===================================================
+bool engine_fire = false;
+bool tilt_abort = false;
+// ===================================================
+// ===               NewPing - SONAR               ===
+// ===================================================
+#define TRIGGER_PIN  12  
+#define ECHO_PIN     11 
+#define MAX_DISTANCE 400 
 
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
-// =============================================
-// ===              BAROMETER                ===
-// =============================================
-BMP280 bmp280;
-SimpleKalmanFilter pressureKalmanFilter(1, 1, 0.01);
+// ===================================================
+// ===               RADIO - NRF24                 ===
+// ===================================================
+RF24 radio(7,8);  CE, CSN          
+RF24Network network(radio); 
 
-static float alt;
-float est_alt;
-float temperature;
-float pascal;
-float base_alt;
+// ===================================================
+// ===                      MPU6050                ===
+// ===================================================
+MPU6050 mpu;
+#define maxrollangle 45
+#define maxpitchangle 45
 
+//gimbal lock
+#define OUTPUT_READABLE_YAWPITCHROLL
+
+#define INTERRUPT_PIN 2  // use pin 2 on Arduino Uno & most boards
+
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+volatile bool mpuInterrupt = false;     
+void dmpDataReady() {
+    mpuInterrupt = true;
+}
+
+// ================================================================
+// ===                      INITIAL SETUP                       ===
+// ================================================================
 
 void setup() {
-    Serial.begin(9600);
-    
-    //Startup Sound
-    tone(buzzer, 2500, 300);
-    delay(1000);
-    tone(buzzer, 2000, 300);
-    delay(500);
-    tone(buzzer, 3000, 300);
-    tone(buzzer, 3500, 300);
-    delay(500);
-    tone(buzzer, 2500, 300);
-    delay(1000);
-    delay(2000);
+    Serial.begin(115200);
+    delay(5000);
 
-    // configure LED for output
+    //Disable Mission Critical Functions:
+    digitalRead(relay, LOW);
 
-    pinMode(GLED, OUTPUT);
-    pinMode(RLED, OUTPUT);
-    pinMode(pyroPin, OUTPUT);
-    digitalWrite(pyroPin, LOW); //Disable for safety
+    //setup MPU6050
+    mpusetup();
 
-    Serial.println(FreeRam()); //set a threshold for that.
-
-      //PASS 1 -- confirm the size.
-  if (FreeRam() < 275) {
-    Serial.println(F("RAM-0"));
-    RED();
-    while(1);
-  }
-
-  //PASS 3: Initialize Baromter
-  //initializeBMP();
-
-  //PASS 4: Pyro Check
-
-  //Get baseline alt
-  float sum = 0;
-  delay(5000);
-  for (int i=0; i < 30; i++) {
-
-    delay(100);
-
-    bmp280.getAltitude(base_alt);
-    sum += base_alt;
-    Serial.println(base_alt);
-  }
-
-  base_alt = sum/30.0;
-  Serial.print(F("BASEH: "));
-  Serial.println(base_alt);
-
-  delay(2000);
-
+    delay(5000);
 }
+
+
+
+// ================================================================
+// ===                    MAIN PROGRAM LOOP                     ===
+// ================================================================
 
 void loop() {
-
-  //Disable Pyros
-  
-  if(release == false && pyro == false && landed == false) {
-    GREEN();
-  }
-  
-               
-
-}
-
-
-
-
-
-
-
-
-// ================================================================
-// ===                RUN FUNCTIONS                      ===
-// ================================================================
-void get_alt() {
-  bmp280.getAltitude(alt);
-  bmp280.getPressure(pascal);
-  est_alt = pressureKalmanFilter.updateEstimate(alt);
-}
-
-
-// ================================================================
-// ===                Initialize FUNCTIONS                      ===
-// ================================================================
-
-// ================================================================
-// ===                         BAROMETER                       ===
-// ================================================================
-
-void initializeBMP() {
-  Serial.print(F("BMP"));
-
-  if (!bmp280.begin()) {
-    Serial.println(F("0"));
-    while (1);
-  }
-
-  /*For drop detection
-    osrs_p = 2
-    osrs_t = 1
-    IIR = 0 (off)
-    t_sb = 0 (0.5ms)
-  */
-
-  /* Default settings from datasheet. */
-  bmp280.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
-                     Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
-                     Adafruit_BMP280::SAMPLING_X1,    /* Pressure oversampling */
-                     Adafruit_BMP280::FILTER_OFF,      /* Filtering. */
-                     Adafruit_BMP280::STANDBY_MS_1); /* Standby time. */
-
-  Serial.println(F("BMPInit1"));
-}
-
-}
-
-
-// ================================================================
-// ===                  MISC FUNCTIONS                          ===
-// ================================================================
-void RED()
-{
-  digitalWrite(RLED, HIGH);
-  tone(buzzer, 2500, 100);
-  delay(200);
-  digitalWrite(RLED, LOW);
-
-  tone(buzzer, 2500, 100);
-  delay(200);
-  digitalWrite(RLED, HIGH);
-
-  tone(buzzer, 2000, 100);
-  delay(500);
-  tone(buzzer, 2000, 100);
-}
-
-void GREEN() {
-  //Everything is fine.. signal.
-  unsigned long interval = 1000;
-  unsigned long currentMillis = millis();
-
-  if (currentMillis - previousMillis > interval) {
-    previousMillis = currentMillis;
+    mpudata();
     
-    digitalWrite(GLED, HIGH);
-    tone(buzzer, 2500, 100);
-  }
-  else {
-    digitalWrite(GLED, LOW);
-  }
+
+    //Tilt Abort Program
+    if (ypr[1] * 180/M_PI > maxrollangle || ypr[1] * 180/M_PI < -maxrollangle) {
+        Serial.println("MAX Roll Tilted!!");
+    }
+    if (ypr[2] * 180/M_PI > maxpitchangle || ypr[2] * 180/M_PI < -maxpitchangle) {
+        Serial.println("MAX Pitch Tilted!!");
+    }
+
+
+    //Descent Engine Fire
+
+    if (sonar.ping_cm())
+
+    if (engine_fire == true && tilt_abort == false && drop == true) {
+
+        digitalRead(relay, HIGH);
+    }
+}
+
+void mpusetup() {
+
+    
+    // initialize device
+    Serial.println(F("Initializing I2C devices..."));
+    mpu.initialize();
+    pinMode(INTERRUPT_PIN, INPUT);
+
+    // verify connection
+    Serial.println(F("Testing device connections..."));
+    Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+
+    //MPU delay 
+    delay(3000);
+
+    // load and configure the DMP
+    Serial.println(F("Initializing DMP..."));
+    devStatus = mpu.dmpInitialize();
+
+    // supply your own gyro offsets here, scaled for min sensitivity
+    mpu.setXGyroOffset(220);
+    mpu.setYGyroOffset(76);
+    mpu.setZGyroOffset(-85);
+    mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+
+    // make sure it worked (returns 0 if so)
+    if (devStatus == 0) {
+        // Calibration Time: generate offsets and calibrate our MPU6050
+        mpu.CalibrateAccel(6);
+        mpu.CalibrateGyro(6);
+        mpu.PrintActiveOffsets();
+        // turn on the DMP, now that it's ready
+        Serial.println(F("Enabling DMP..."));
+        mpu.setDMPEnabled(true);
+
+        // enable Arduino interrupt detection
+        Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
+        Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
+        Serial.println(F(")..."));
+        attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+        mpuIntStatus = mpu.getIntStatus();
+
+        // set our DMP Ready flag so the main loop() function knows it's okay to use it
+        Serial.println(F("DMP ready! Waiting for first interrupt..."));
+        dmpReady = true;
+
+        // get expected DMP packet size for later comparison
+        packetSize = mpu.dmpGetFIFOPacketSize();
+    } else {
+        // ERROR!
+        // 1 = initial memory load failed
+        // 2 = DMP configuration updates failed
+        // (if it's going to break, usually the code will be 1)
+        Serial.print(F("DMP Initialization failed (code "));
+        Serial.print(devStatus);
+        Serial.println(F(")"));
+    }
+
+}
+
+void mpudata() {
+    // if programming failed, don't try to do anything
+    if (!dmpReady) return;
+
+    // wait for MPU interrupt or extra packet(s) available
+    while (!mpuInterrupt && fifoCount < packetSize) {
+        if (mpuInterrupt && fifoCount < packetSize) {
+          // try to get out of the infinite loop 
+          fifoCount = mpu.getFIFOCount();
+        }  
+        // other program behavior stuff here
+        // .
+        // .
+        // .
+        // if you are really paranoid you can frequently test in between other
+        // stuff to see if mpuInterrupt is true, and if so, "break;" from the
+        // while() loop to immediately process the MPU data
+        // .
+        // .
+        // .
+    }
+
+    // reset interrupt flag and get INT_STATUS byte
+    mpuInterrupt = false;
+    mpuIntStatus = mpu.getIntStatus();
+
+    // get current FIFO count
+    fifoCount = mpu.getFIFOCount();
+	if(fifoCount < packetSize){
+	        //Lets go back and wait for another interrupt. We shouldn't be here, we got an interrupt from another event
+			// This is blocking so don't do it   while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+	}
+    // check for overflow (this should never happen unless our code is too inefficient)
+    else if ((mpuIntStatus & _BV(MPU6050_INTERRUPT_FIFO_OFLOW_BIT)) || fifoCount >= 1024) {
+        // reset so we can continue cleanly
+        mpu.resetFIFO();
+      //  fifoCount = mpu.getFIFOCount();  // will be zero after reset no need to ask
+        Serial.println(F("FIFO overflow!"));
+
+    // otherwise, check for DMP data ready interrupt (this should happen frequently)
+    } else if (mpuIntStatus & _BV(MPU6050_INTERRUPT_DMP_INT_BIT)) {
+
+        // read a packet from FIFO
+	while(fifoCount >= packetSize){ // Lets catch up to NOW, someone is using the dreaded delay()!
+		mpu.getFIFOBytes(fifoBuffer, packetSize);
+		// track FIFO count here in case there is > 1 packet available
+		// (this lets us immediately read more without waiting for an interrupt)
+		fifoCount -= packetSize;
+	}
+        #ifdef OUTPUT_READABLE_YAWPITCHROLL
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetGravity(&gravity, &q);
+            mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+            // Serial.print("ypr\t");
+            // Serial.print(ypr[0] * 180/M_PI);
+            // Serial.print("\t");
+            // Serial.print(ypr[1] * 180/M_PI);
+            // Serial.print("\t");
+            // Serial.println(ypr[2] * 180/M_PI);
+        #endif
+        
+    }
 }
